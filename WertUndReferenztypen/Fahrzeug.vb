6d6f4fe9297dcb39c39a696754028160c639f2eb
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-10-05 11:40:24 +0200

    Wert und Referenztypen

---
+++ WertUndReferenztypen/Fahrzeug.vb
@@ -0,0 +1,13 @@
+﻿Public Class Fahrzeug
+
+    Private _geschwindigkeit As Integer
+    Public Property Geschwindigkeit As Integer
+        Get
+            Return _geschwindigkeit
+        End Get
+        Set(ByVal value As Integer)
+            _geschwindigkeit = value
+        End Set
+    End Property
+
+End Class
---
+++ WertUndReferenztypen/Module1.vb
@@ -0,0 +1,43 @@
+﻿Module Module1
+
+    Sub Main()
+        ' Objekte von Strukturen sind immer Werttypen
+        ' Objekte von Klassen sind immer Referenztypen
+        ' Einzige Ausnahme: class String!
+
+        Console.WriteLine("----- Werttypen -----")
+
+        Dim i As Integer = 10
+        Dim j As Integer = i
+
+        Console.WriteLine($"I: {i}")
+        Console.WriteLine($"J: {j}")
+
+        Console.WriteLine("J ändern")
+        j = 50
+        Console.WriteLine($"I: {i}")
+        Console.WriteLine($"J: {j}")
+
+
+        Console.WriteLine($"{vbNewLine}{vbNewLine}")
+
+        Console.WriteLine("----- Referenztypen -----")
+
+        Dim pkw As Fahrzeug = New Fahrzeug()
+        pkw.Geschwindigkeit = 80
+        Dim bus As Fahrzeug = pkw
+
+        Console.WriteLine($"PKW: {pkw.Geschwindigkeit}")
+        Console.WriteLine($"Bus: {bus.Geschwindigkeit}")
+
+        Console.WriteLine("Bus beschleunigen")
+        bus.Geschwindigkeit = 120
+
+        Console.WriteLine($"PKW: {pkw.Geschwindigkeit}")
+        Console.WriteLine($"Bus: {bus.Geschwindigkeit}")
+
+
+        Console.ReadLine()
+    End Sub
+
+End Module
