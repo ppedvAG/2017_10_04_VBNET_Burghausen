e0952d632e8054c42acf975cb059370e597a06a1
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-10-06 11:26:47 +0200

    Exceptions

---
+++ Exceptions/Module1.vb
@@ -0,0 +1,40 @@
+﻿Option Strict On
+
+Module Module1
+
+    Sub Main()
+
+        Try
+
+            Dim zahl1 = Integer.Parse(Console.ReadLine())
+            Dim zahl2 = Integer.Parse(Console.ReadLine())
+
+            If zahl1 = 6 OrElse zahl2 = 6 Then
+                Throw New MustNotBe6Exception("Es darf nicht 6 eingegeben werden!!")
+            End If
+
+            Dim ergebnis = zahl1 + zahl2
+
+            Console.WriteLine(ergebnis)
+
+        Catch ex As FormatException
+
+            Console.WriteLine("Bitte nur Zahlen eingeben.")
+            Console.WriteLine(ex.Message)
+            Console.WriteLine(ex.StackTrace)
+
+        Catch ex As OverflowException
+
+            Console.WriteLine("Bitte keine zu großen Zahlen eingeben.")
+            Console.WriteLine(ex.Message)
+
+        Catch ex As Exception
+            Console.WriteLine(ex.Message)
+
+        End Try
+
+        Console.ReadLine()
+
+    End Sub
+
+End Module
---
+++ Exceptions/MustNotBe6Exception.vb
@@ -0,0 +1,8 @@
+﻿Public Class MustNotBe6Exception
+    Inherits Exception
+
+    Public Sub New(message As String)
+        MyBase.New(message)
+    End Sub
+
+End Class
